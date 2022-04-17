ad3345e9eebf7098d80bafdc3ddcb2ca32c4a1f5
Abhishek <abhishek.karmacharya@jeeveehealth.com.np>
2022-04-17 08:05:57 +0545

    day 2 and cmd argument added during execution

--- src/days/dive.rs
+++ src/days/dive.rs
@@ -1,19 +1,7 @@
 use std::fs;
 
 //link to question
-//https://adventofcode.com/2021/day/1
-
-// struct ICommand{
-//     Forward:String,
-//     Down:String,
-//     Up:String
-// }
-
-enum Commands{
-    Forward,
-    Down,
-    Up
-}
+//https://adventofcode.com/2021/day/2
 
 pub fn day_2() {
     let report = match fs::read_to_string("./datas/dive.txt") {
@@ -22,18 +10,22 @@ pub fn day_2() {
             panic!("something went wrong {}", error);
         },
     };
-    println!("{:?}", report);
-    // let formatted :Vec<&str> = report.trim().split("\n").collect();
-    // for (pos, data) in formatted.iter().enumerate(){
-    //     let parsed_string =  data.parse().unwrap();
-    //     if pos == 0{
-    //         old_value = parsed_string;
-    //     }else{
-    //         if parsed_string > old_value {
-    //             increased += 1;
-    //         } 
-    //         old_value = parsed_string;
-    //     }
-    // }
-    // println!("Measurement increased by {}", increased)
+    let formatted :Vec<&str> = report.trim().split("\n").collect();
+    let mut position = 0;
+    let mut depth = 0;
+    for (pos, data) in formatted.iter().enumerate(){
+        let mut iter = data.splitn(2, ' ');
+        let command = iter.next().unwrap();
+        let value: u32 = iter.next().unwrap().parse().unwrap();
+        if command == "forward"{
+            position += value;
+        }else if command == "down"  {
+            depth += value 
+        }else{
+           depth -= value 
+        } 
+    }
+    println!("Position is {:?} and depth is {:?}", position, depth);
+    let exact_positions = position * depth;
+    println!("exact postions is {:?}", exact_positions);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::fs;
+use core::panic;
+use std::env;
 mod days;
 
 
 //link to question
 //https://adventofcode.com/2021/day/1
 
+
 fn main() {
-    // days::sonar_sweep::day_1();
-    days::dive::day_2();
+    let arg = env::args().nth(1).unwrap();
+    match arg.as_str() {
+        "day-1" => days::sonar_sweep::day_1(),
+        "day-2" => days::dive::day_2(),
+        _ => panic!("Invalid arguments")
+    }
 }
